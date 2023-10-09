fe7ee1b99d0a226035e3f16c0ec6ed048bf8bd5f
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-10-09 16:41:43 -0300

    Added navbar data

---
+++ lib/data.ts
@@ -0,0 +1,61 @@
+import { FaCrown } from "react-icons/fa";
+import { HiOutlineHome } from "react-icons/hi";
+import { BsDiagram3, BsBookmarks } from "react-icons/bs";
+import { AiOutlineTags } from "react-icons/ai";
+import { LiaUserShieldSolid } from "react-icons/lia";
+import { BiStore } from "react-icons/bi";
+import { PiGearSix } from "react-icons/pi";
+import { FiUsers, FiLogOut } from "react-icons/fi";
+
+export const navbarInfo = [
+    {
+        label: "Cantinho da Moda",
+        url: "/",
+        icon: FaCrown
+    },
+    {
+        label: "Dashboard",
+        url: "/dashboard",
+        icon: HiOutlineHome
+    },
+    {
+        label: "Categories",
+        url: "/categories",
+        icon: BsDiagram3
+    },
+    {
+        label: "Brands",
+        url: "/brands",
+        icon: BsBookmarks
+    },
+    {
+        label: "Products",
+        url: "/products",
+        icon: AiOutlineTags
+    },
+    {
+        label: "Clients",
+        url: "/clients",
+        icon: FiUsers
+    },
+    {
+        label: "Users",
+        url: "/users",
+        icon: LiaUserShieldSolid
+    },
+    {
+        label: "Companies",
+        url: "/companies",
+        icon: BiStore
+    }, 
+    {
+        label: "Settings",
+        url: "/settings",
+        icon: PiGearSix
+    }, 
+    {
+        label: "Log Out",
+        url: "/logout",
+        icon: FiLogOut
+    }, 
+] as const;
