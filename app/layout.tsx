a31d0fbfa6982cd4879871ff7a2ca548a83ceba1
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-10-11 15:45:59 -0300

    Added contex provider to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,10 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
 
+import ContextProvider from '@/context/ContextProvider';
+
 import Header from '@/components/Header';
 import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
 
 const font = Roboto({ subsets: ["latin"], display: "swap", weight: ["300", "400", "500", "700"] });
 
@@ -17,10 +18,11 @@ export default function RootLayout( {children}: {children: React.ReactNode} ) {
   return (
     <html lang="pt-br">
       <body className={font.className} suppressHydrationWarning={true}>
-        <Navbar />
-        <Header />
-        {children}
-        <Footer />
+        <ContextProvider>
+          <Navbar />
+          <Header />
+          {children}
+        </ContextProvider>
       </body>
     </html>
   );
