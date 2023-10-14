3489bfcd91e818d757d8a1ff5f93d04e9b4f505f
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-10-14 16:57:58 -0300

    Created theme context provider

--- context/ContextProvider.tsx
+++ context/ContextProvider.tsx
@@ -3,14 +3,17 @@
 import { NextUIProvider } from "@nextui-org/react";
 
 import NavbarContextProvider from "./NavbarContext";
+import ThemeContextProvider from "./ThemeContext";
 
 const ContextProvider = ( {children}: { children: React.ReactNode } ) => {
 
     return (
         <NextUIProvider>
-            <NavbarContextProvider>
-                {children}
-            </NavbarContextProvider>
+            <ThemeContextProvider>
+                <NavbarContextProvider>
+                    {children}
+                </NavbarContextProvider>
+            </ThemeContextProvider>
         </NextUIProvider>
     );
 }
