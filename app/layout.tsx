5c84da90f6abca73645893d1c657efdca1dc493e
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-11-07 04:51:27 -0300

    RootLayout simplified

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,6 @@ import { Roboto } from 'next/font/google';
 import ContextProvider from '@/providers/ContextProvider';
 import ToastProvider from '@/providers/ToastProvider';
 
-import Header from "@/components/Header";
-import Navbar from "@/components/Navbar";
-
 import './globals.css';
 
 const font = Roboto({ subsets: ["latin"], display: "swap", weight: ["300", "400", "500", "700"] });
@@ -23,13 +20,7 @@ export default function RootLayout( {children}: {children: React.ReactNode} ) {
         <ContextProvider>
           <ToastProvider />
           <div className="flex relative h-screen bg-gray-50 dark:bg-neutral-900">
-            <Navbar />
-            <main className="flex flex-col w-full gap-y-3 my-2 mx-3">
-              <Header />
-              <section className="mx-5">
-                {children}
-              </section>
-            </main>
+            {children}
           </div>
         </ContextProvider>
       </body>
