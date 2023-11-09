b9b144bc63160c946c54b6ce9fdc961bd31fbfa2
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-11-08 22:48:09 -0300

    Changed login page

--- app/api/(auth)/auth/signin/page.tsx
+++ app/api/(auth)/auth/signin/page.tsx
@@ -1,10 +1,10 @@
-import FormLogin from "@/components/forms/FormLogin";
+import LoginForm from "@/components/forms/LoginForm";
 
 const LoginPage = () => {
   return (
     <section className="bg-border absolute flex flex-col items-center justify-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/3 py-6 px-5 rounded-md">
         <h1 className="mb-5">Login</h1>
-        <FormLogin />
+        <LoginForm />
     </section>
   );
 };
