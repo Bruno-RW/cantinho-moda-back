0ddd11fb64551e810b36a6bf22fda216dbc44d34
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-11-08 22:57:49 -0300

    Created API route for new users page

---
+++ app/api/(dashboard)/users/new/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import { hash } from "bcrypt";
+
+import db from "@/lib/db";
+import { newUserFormSchema } from "@/lib/types/forms";
+
+export async function POST(req: Request) {
+  try {
+    const body: unknown = await req.json();
+    const { fullName, email, type, password } = newUserFormSchema.parse(body);
+
+    const existingUserByEmail = await db.user.findUnique({ where: { email } });
+
+    if (existingUserByEmail) return NextResponse.json("E-mail already exists", { status: 400 });
+
+    const hashedPassword = await hash(password, 10);
+
+    await db.user.create({
+      data: {
+        password: hashedPassword,
+        fullName,
+        email,
+        type,
+      }
+    });
+
+    return NextResponse.json("Admin user created successfully", { status: 201 });
+
+  } catch (error: any) {
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
