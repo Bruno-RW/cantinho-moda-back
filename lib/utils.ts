2be29f8270ffc2aa4f36073eff624062377b4ee2
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-11-08 22:50:12 -0300

    Created getInitials function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getInitials(fullName: string) {
+  const nameArray = fullName.split(" ");
+
+  const firstName = nameArray[0].charAt(0).toUpperCase();
+  const lastName = nameArray[nameArray.length - 1].charAt(0).toUpperCase();
+
+  return firstName + lastName;
+}
