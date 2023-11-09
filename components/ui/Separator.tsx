4cf698d53f57ff0424ed9573dfba8dda5d3ddffa
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-11-09 18:31:50 -0300

    Added margin bottom to default style

--- components/ui/Separator.tsx
+++ components/ui/Separator.tsx
@@ -3,6 +3,6 @@ import { cn } from "@/lib/utils";
 interface SeparatorProps { className?: string };
 
 const Separator: React.FC<SeparatorProps> = ({ className }) => {
-  return <hr className={cn("w-full border-black/10 dark:border-white/10", className)} />
+  return <hr className={cn("w-full mb-3 border-black/10 dark:border-white/10", className)} />
 }
 export default Separator;
