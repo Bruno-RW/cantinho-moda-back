8e993154fb41c39c6fffa74d871dd8d133de8e3b
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-11-09 19:36:41 -0300

    Middleware major improvement

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,35 @@
-export { default } from "next-auth/middleware";
+import { NextRequestWithAuth, withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware(request: NextRequestWithAuth) {
+    if (request.nextUrl.pathname.startsWith("/users") && request.nextauth.token?.type !== "M") {
+      return NextResponse.redirect( new URL("/", request.url) );
+    }
+
+    if (request.nextUrl.pathname.startsWith("/companies") && request.nextauth.token?.type !== "M") {
+      return NextResponse.redirect( new URL("/", request.url) );
+    }
+
+    const origin = request.nextUrl.origin;
+    const pathname = request.nextUrl.pathname;
+    const requestHeaders = new Headers(request.headers);
+    
+    requestHeaders.set("x-url", request.url);
+    requestHeaders.set("x-origin", origin);
+    requestHeaders.set("x-pathname", pathname);
+
+    return NextResponse.next({
+      request: { headers: requestHeaders }
+    });
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+);
 
 export const config = {
-    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/((?!api/).*)"]
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/((?!api/).*)"],
 };
