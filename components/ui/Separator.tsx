fdbfac7eb11992c0eebba71deb40eace3717d37d
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-11-08 22:49:11 -0300

    Added optional className

--- components/ui/Separator.tsx
+++ components/ui/Separator.tsx
@@ -1,4 +1,8 @@
-const Separator = () => {
-  return <hr className="w-full border-black/10 dark:border-white/10" />
+import { cn } from "@/lib/utils";
+
+interface SeparatorProps { className?: string };
+
+const Separator: React.FC<SeparatorProps> = ({ className }) => {
+  return <hr className={cn("w-full border-black/10 dark:border-white/10", className)} />
 }
 export default Separator;
