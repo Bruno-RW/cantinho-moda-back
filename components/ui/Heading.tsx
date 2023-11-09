51c0fcd2c9e1f4931240b5fc2187995dbf2f5f19
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-11-08 22:49:23 -0300

    Added more options

--- components/ui/Heading.tsx
+++ components/ui/Heading.tsx
@@ -1,18 +1,32 @@
 import { cn } from "@/lib/utils";
 
 interface HeadingProps {
-  title: string;
+  title?: string;
   subtitle?: string;
+  description?: string;
   
   titleClass?: string;
   subtitleClass?: string;
+  descriptionClass?: string;
 }
 
-const Heading: React.FC<HeadingProps> = ({ title, subtitle, titleClass, subtitleClass }) => {
+const Heading: React.FC<HeadingProps> = ({ title, subtitle, description, titleClass, subtitleClass, descriptionClass }) => {
   return (
     <div className="flex flex-col">
-      <h1 className={cn("text-4xl", subtitle && "mb-1", titleClass)}>{title}</h1>
-      <h2 className={cn("text-lg font-medium text-black/50 dark:font-normal dark:text-white/50", subtitleClass)}>{subtitle}</h2>
+      {title && (
+        <>
+          <h1 className={cn("text-4xl", description && "mb-1", titleClass)}>{title}</h1>
+          <span className={cn("text-lg font-medium text-black/50 dark:font-normal dark:text-white/50", descriptionClass)}>{description}</span>
+        </>
+      )}
+
+      {subtitle && (
+        <>
+          <h2 className={cn("text-3xl", description && "mb-1", subtitleClass)}>{subtitle}</h2>
+          <span className={cn("text-lg font-medium text-black/50 dark:font-normal dark:text-white/50", descriptionClass)}>{description}</span>
+        </>
+      )}
+
     </div>
   );
 }
