5fd3f9b75696a3615d9d445df78ecbf3859b94b6
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-11-11 21:58:06 -0300

    Created getUrl function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
+import { ReadonlyHeaders } from "next/dist/server/web/spec-extension/adapters/headers";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -12,4 +13,12 @@ export function getInitials(fullName: string) {
   const lastName = nameArray[nameArray.length - 1].charAt(0).toUpperCase();
 
   return firstName + lastName;
+}
+
+export function getUrl(headers: ReadonlyHeaders) {
+  const url = headers.get('x-url') as string;
+  const pathname = headers.get('x-pathname') as string;
+  const origin = headers.get('x-origin') as string;
+
+  return { url, pathname, origin };
 }
