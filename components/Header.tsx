678c538381d688efb64809a7753c4638b0969982
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-11-12 17:32:03 -0300

    Added Breadcrumbs to Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,17 @@
 import NavbarButton from "@/components/header/NavbarButton";
+import Breadcrumbs from "@/components/header/Breadcrumbs";
 import UserAvatar from "@/components/header/UserAvatar";
 import Search from "@/components/header/Search";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between z-40 w-full h-14 transition-all">
-      <NavbarButton />
-      <Search />
-      <UserAvatar />
+    <header className="flex flex-col gap-y-2 z-40 w-full transition-all">
+      <div className="flex items-center justify-between py-3">
+        <NavbarButton />
+        <Search />
+        <UserAvatar />
+      </div>
+      <Breadcrumbs />
     </header>
   );
 }
