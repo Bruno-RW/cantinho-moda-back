91f1f08a78d015e7dc3b4b0294e462584f2bc2b6
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-11-11 21:58:57 -0300

    Separated routes to its own component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,14 @@
 "use client";
 
-import Link from "next/link";
 import { FaCrown } from "react-icons/fa";
-import { signOut } from "next-auth/react";
 
 import { cn } from "@/lib/utils";
-import { navbarInfo } from "@/lib/data";
 import { useNavbar } from "@/context/NavbarContext";
 
-import Badge from "@/components/ui/Badge";
+import MappedRoutes from "@/components/navbar/MappedRoutes";
 
 const Navbar = () => {
   const { isActive } = useNavbar();
-
-  const onClick = (item: any) => { if (item.signOut) signOut() };
 
   return (
     <nav className={cn("w-72 h-screen overflow-hidden transition-all bg-blue-600 dark:bg-blue-500/80", isActive && "w-[60px] active")}>
@@ -22,32 +17,7 @@ const Navbar = () => {
         <span className="pt-1 whitespace-nowrap">{isActive ? "" : "Cantinho da Moda"}</span>
       </div>
 
-      <ul className="w-full">
-        {navbarInfo.map((item, key) => (
-          <li className={cn("navbar-li group", isActive && "hover:translate-x-0 hover:pl-2 hover:ml-2")} key={key} onClick={() => onClick(item)}>
-            <Link className="flex items-center relative text-gray-50 w-full gap-x-3.5 py-3.5 no-underline group-hover:text-blue-600 dark:text-neutral-50 dark:group-hover:text-blue-500/80" href={item.url}>
-
-              <span className={cn("link-before absolute -top-[51px] right-[11px] -z-50 content-[''] w-[50px] h-[50px] rounded-full pointer-events-none bg-transparent", isActive && "-right-[1px]")} />
-
-              <item.icon className="relative" size={28} />
-              {item.type === "M" ? (
-                <span className="relative flex items-center justify-center whitespace-nowrap">
-                  {isActive ? "" : (
-                    <>
-                      {item.label}
-                      <Badge className="ml-2 group-hover:text-green-600 dark:group-hover:text-green-300" variant="green">Master</Badge>
-                    </>
-                  )}
-                </span>
-              ) : (
-                <span className="relative whitespace-nowrap">{isActive ? "" : item.label}</span>
-              )}
-
-              <span className={cn("link-after absolute -bottom-[51px] right-[11px] -z-50 content-[''] w-[50px] h-[50px] rounded-full pointer-events-none bg-transparent", isActive && "-right-[1px]")} />
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <MappedRoutes isActive={isActive} />
     </nav>
   );
 }
---
+++ components/navbar/MappedRoutes.tsx
@@ -0,0 +1,63 @@
+import { signOut, useSession } from "next-auth/react";
+import toast from "react-hot-toast";
+import Link from "next/link";
+
+import { navbarInfo } from "@/lib/data";
+import { cn } from "@/lib/utils";
+
+import Badge from "@/components/ui/Badge";
+
+interface MappedRoutesProps { isActive: boolean };
+
+const MappedRoutes: React.FC<MappedRoutesProps> = ({ isActive }) => {
+  const { data: session } = useSession();
+  const isMaster = session?.user.type === "M";
+
+  const onClick = (item: any) => {
+    if (item.signOut) signOut();
+
+    if (!isMaster && item.type === "M") toast.error("Unauthorized");
+  };
+
+  return (
+    <ul className="w-full">
+      {navbarInfo.map((item, key) => (
+        <li className={cn("navbar-li group", isActive && "hover:translate-x-0 hover:pl-2 hover:ml-2")} key={key} onClick={() => onClick(item)}>
+          <Link className="flex items-center relative text-gray-50 w-full gap-x-3.5 py-3.5 no-underline group-hover:text-blue-600 dark:text-neutral-50 dark:group-hover:text-blue-500/80" href={item.url}>
+
+            <span className={cn("link-before absolute -top-[51px] right-[11px] -z-50 content-[''] w-[50px] h-[50px] rounded-full pointer-events-none bg-transparent", isActive && "-right-[1px]")} />
+
+            <item.icon className="relative" size={28} />
+
+            {item.type === "M" ? (
+              <span className="relative flex items-center justify-center whitespace-nowrap">
+                {isActive ? ("") : (
+                  <>
+                    {item.label}
+                      
+                    {isMaster ? (
+                      <Badge className="ml-2 group-hover:text-green-600 dark:group-hover:text-green-300" variant="green">
+                        Master
+                      </Badge>
+                    ) : (
+                      <Badge className="ml-2 group-hover:text-red-600 dark:group-hover:text-red-300" variant="red">
+                        Master
+                      </Badge>
+                    )}
+                  </>
+                )}
+              </span>
+            ) : (
+              <span className="relative whitespace-nowrap">
+                {isActive ? "" : item.label}
+              </span>
+            )}
+
+            <span className={cn("link-after absolute -bottom-[51px] right-[11px] -z-50 content-[''] w-[50px] h-[50px] rounded-full pointer-events-none bg-transparent", isActive && "-right-[1px]")} />
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+export default MappedRoutes;
