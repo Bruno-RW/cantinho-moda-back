6b21c52e6c5f38b075dbf2f0f0d60b0b8cb8023d
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-11-13 22:29:06 -0300

    Toast fix

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import toast from "react-hot-toast";
+
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  toast.remove();
+  
   return (
     <>
       <Navbar />
@@ -11,5 +17,5 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
       </main>
     </>
   );
-};
+}
 export default DashboardLayout;
