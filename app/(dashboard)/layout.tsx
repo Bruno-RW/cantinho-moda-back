2dc61886a5b23d1c6205d4f790e0f7ddfc222243
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-11-15 16:19:57 -0300

    Background layout fix

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import toast from "react-hot-toast";
-
 import { useNavbar } from "@/context/NavbarContext";
 import { cn } from "@/lib/utils";
 
@@ -10,12 +8,11 @@ import Navbar from "@/components/Navbar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   const { isActive } = useNavbar();
-  toast.remove();
   
   return (
     <>
       <Navbar isActive={isActive} />
-      <main className={cn("flex flex-col gap-y-3 my-2 ml-[300px] mr-3 w-full transition-all", isActive && "ml-[72px]")}>
+      <main className={cn("flex flex-col gap-y-3 py-2 pl-[272px] pr-3 w-full h-[200%] transition-all bg-gray-50 dark:bg-neutral-900", isActive && "pl-[72px]")}>
         <Header />
         <section className="flex flex-col gap-y-3 mx-5">{children}</section>
       </main>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,29 @@
-import type { Metadata } from 'next';
-import { Roboto } from 'next/font/google';
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
 
-import ContextProvider from '@/providers/ContextProvider';
+import ContextProvider from "@/providers/ContextProvider";
 
-import './globals.css';
+import "./globals.css";
 
 const font = Roboto({ subsets: ["latin"], display: "swap", weight: ["300", "400", "500", "700"] });
 
 export const metadata: Metadata = {
-  title: 'Cantinho da Moda | ADMIN',
-  description: 'Administre os produtos e informações da sua loja',
+  title: "Cantinho da Moda | ADMIN",
+  description: "Administre os produtos e informações da sua loja",
 }
 
 export default function RootLayout( {children}: {children: React.ReactNode} ) {
+  const session = getServerSession();
+
+  if (!session) return NextResponse.redirect("/api/auth/sigin");
+
   return (
-    <html lang="pt-br">
+    <html lang="en-US">
       <body className={font.className} suppressHydrationWarning={true}>
         <ContextProvider>
-          <div className="flex relative w-screen h-screen overflow-hidden bg-gray-50 dark:bg-neutral-900">
+          <div className="flex relative w-full h-screen bg-gray-50 dark:bg-neutral-900">
             {children}
           </div>
         </ContextProvider>
