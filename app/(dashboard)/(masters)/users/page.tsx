0a519a8abc25d8aad0beec25c65c956661f16d00
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-11-19 17:34:12 -0300

    Added users data table and formatted data

--- app/(dashboard)/(masters)/users/page.tsx
+++ app/(dashboard)/(masters)/users/page.tsx
@@ -1,14 +1,21 @@
 import db from "@/lib/db";
+import { formatDateTime } from "@/lib/utils";
 
 import UsersData from "@/components/tables/users/UsersData";
+import { UsersColumnProps } from "@/components/tables/users/UsersColumn";
 
 const UsersPage = async () => {
-  const users = await db.user.findMany({
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
+  const users = await db.user.findMany();
 
-  return <UsersData data={users} />;
+  const formattedUsers: UsersColumnProps[] = users.map(user => ({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    type: user.type,
+    createdAt: formatDateTime(user.createdAt),
+    updatedAt: formatDateTime(user.updatedAt)
+  }));
+
+  return <UsersData data={formattedUsers} />;
 }
 export default UsersPage;
