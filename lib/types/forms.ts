ee88704e8d37edd276f31110f977b55e9c899961
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-11-27 11:18:12 -0300

    Created clientFormSchema

--- lib/types/forms.ts
+++ lib/types/forms.ts
@@ -101,6 +101,44 @@ export const userFormSchema = z
   .refine(({ password, confirmPassword }) => password === confirmPassword, {
     message: "Password does not match",
     path: ["confirmPassword"]
-  }
-);
+  });
 export type userFormData = z.infer<typeof userFormSchema>;
+
+export const clientFormSchema = z
+  .object({
+    name: z
+      .string()
+      .trim()
+      .min(1, "Name is required")
+      .min(5, "Name must have atleast 5 characters")
+      .max(70, "Name must be shorter than 70 characters"),
+
+    email: z
+      .string()
+      .trim()
+      .min(1, "E-mail is required")
+      .min(10, "E-mail must have atleast 10 characters")
+      .max(70, "E-mail must be shorter than 70 characters")
+      .email("Invalid e-mail")
+      .toLowerCase(),
+
+    password: z
+      .string()
+      .trim()
+      .min(1, "Password is required")
+      .min(8, "Password must have atleast 8 characters")
+      .max(30, "Password must be shorter than 30 characters"),
+
+    confirmPassword: z
+      .string()
+      .trim()
+      .min(1, "Password confirmation is required")
+      .min(8, "Password confirmation must have atleast 8 characters")
+      .max(30, "Password confirmation must be shorter than 30 characters")
+      .optional()
+  })
+  .refine(({ password, confirmPassword }) => password === confirmPassword, {
+    message: "Password does not match",
+    path: ["confirmPassword"]
+  });
+export type clientFormData = z.infer<typeof clientFormSchema>;
